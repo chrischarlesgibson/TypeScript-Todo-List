5e2b32541ddc3270b16df47a117e21c660882219
Chris Gibson <chrischarlesgibson@gmail.com>
2023-03-23 17:05:11 -0500

    added checkbox functionality and localstorage functionality

--- src/index.ts
+++ src/index.ts
@@ -13,7 +13,10 @@ const list = document.querySelector<HTMLUListElement>("#list");
 const form = document.getElementById("new-rask-form") as HTMLFormElement | null;
 const input = document.querySelector<HTMLInputElement>("#new-task-title");
 
-form?.addEventListener("submit", (e) => {
+//tasks varibale should be an array of Tasks
+const tasks: Task[] = getTasks();
+tasks.forEach(addListItem);
+addEventListener("submit", (e) => {
   e.preventDefault();
 
   if (input?.value == "" || input?.value == null) return;
@@ -26,6 +29,8 @@ form?.addEventListener("submit", (e) => {
     createdAt: new Date(),
   };
 
+  tasks.push(newTask);
+
   //or can be explicit and do
   // const newTask: Task = {
   //   id: uuid4(),
@@ -35,13 +40,32 @@ form?.addEventListener("submit", (e) => {
   // };
 
   addListItem(newTask);
+  //clear input when add new task
+  input.value = "";
 });
 function addListItem(task: Task) {
   const item = document.createElement("li");
   const label = document.createElement("label");
   const checkbox = document.createElement("input");
+  checkbox.addEventListener("change", () => {
+    task.completed = checkbox.checked;
+    console.log(tasks);
+    saveTasks();
+  });
   checkbox.type = "checkbox";
+  checkbox.checked = task.completed;
   label.append(checkbox, task.title);
   item.append(label);
   list?.append(item);
+}
+
+function saveTasks() {
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+}
+
+//explicitly tell it what should be returned. it should reutrn array of Tasks
+function getTasks(): Task[] {
+  const taskJSON = localStorage.getItem("tasks");
+  if (taskJSON == null) return [];
+  return JSON.parse(taskJSON);
 }
